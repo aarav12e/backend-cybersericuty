f14d512e402b04966c79cbf19c8f08845776d9e8
Aarav Kumar <aarav12f@gmail.com>
2026-05-14 10:03:15 +0530

    server location updated

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 /* =========================
    MONGODB CONNECTION
